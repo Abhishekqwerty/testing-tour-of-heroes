50542d89d7662e14b57f1fe2032763a25b5de8c0
Abhishek <gaikwadabhishek978@gmail.com>
2020-03-18 11:21:43 +0530

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,9 +8,15 @@ exports.config = {
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
+//   capabilities: {
+//     'browserName': 'chrome'
+//   },
   capabilities: {
-    'browserName': 'chrome'
+  chromeOptions: {
+    args: [ "--headless" ]
   },
+  'browserName': 'chrome'
+},
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
